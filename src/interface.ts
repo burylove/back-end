afd13bcae7b97fb519b1bb5929ec6fd68472f045
henryliu <17600047539@163.com>
2022-04-28 21:43:34 +0800

    update

--- src/interface.ts
+++ src/interface.ts
@@ -1,7 +1,15 @@
-/**
- * @description User-Service parameters
- */
 export interface IUserOptions {
   uid: number;
   url: string;
+}
+
+export interface Web2UserAndKey {
+  description?:string;
+  email: string;
+  publicKey:string;
+  secretKey:string;
+}
+
+export interface Web2UserKey {
+  key:string
 }

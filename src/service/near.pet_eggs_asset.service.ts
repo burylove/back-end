641a8c9df913b8547110e39d26b06cd78305d78d
henryliu <17600047539@163.com>
2022-05-02 17:10:18 +0800

    update

--- src/service/near.pet_eggs_asset.service.ts
+++ src/service/near.pet_eggs_asset.service.ts
@@ -3,6 +3,7 @@ import { InjectEntityModel } from '@midwayjs/orm';
 import { Repository } from 'typeorm';
 import {near_pet_eggs_asset} from "../entity/near_pet_eggs_asset";
 import {Pet_eggs_number} from "../entity/pet_eggs_number";
+import {Uncommon_pet_random} from "../utils/random";
 
 @Provide()
 export class NearUsersPetEggsAssetService {
@@ -19,10 +20,11 @@ export class NearUsersPetEggsAssetService {
     const pet_eggs_number_result = await this.pet_eggs_number_Model.findOne({
       where: { network:"near" },
     });
+    const type = await Uncommon_pet_random()
     user.near_pet_eggs_index = pet_eggs_number_result.pet_eggs_number
     user.near_pet_eggs_name = 'able'
     user.near_pet_eggs_image_url = 'https://cdn.discordapp.com/attachments/876498266550853642/970292528224038952/12.png'
-    user.near_pet_eggs_type = 'normal'
+    user.near_pet_eggs_type = type
     // save entity
     const userResult = await this.usersModel.save(user);
     pet_eggs_number_result.pet_eggs_number ++
